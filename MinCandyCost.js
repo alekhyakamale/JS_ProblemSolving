65af21fc4d7d0636c3841926cbc3193aabde3d7b
alekhyakamale <alekhyakamale39@gmail.com>
2026-06-01 23:29:01 +0530

    Minimum candy cost - Leedcode daily

---
+++ MinCandyCost.js
@@ -0,0 +1,14 @@
+const arr = [6,5,7,9,2,2];
+
+function MinCandyCost(arr){
+    arr.sort((a,b) => b-a);
+    let totalCount = 0;
+    for(let i=0; i<arr.length; i++){
+        if((i+1)%3 !==0) {
+            totalCount+=arr[i];
+        }
+    }
+    return totalCount;
+}
+
+console.log(MinCandyCost(arr));
